86243d696ddad9980e230991bacef6868dccbec1
geely <minwang_life@163.com>
2023-05-05 20:13:03 +0800

    first commit

---
+++ build/CMakeFiles/testqjj.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testqjj.dir/test.cpp.o"
+  "CMakeFiles/testqjj.dir/test.cpp.o.d"
+  "testqjj"
+  "testqjj.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testqjj.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
